5fedd0513917141b21eb152bf66cec8d7e91e54a
NicoRahm <nicolas.rahmouni@polytechnique.edu>
2016-11-13 17:45:52 +0100

    Taking into acount a list data structure

--- main.py
+++ main.py
@@ -6,30 +6,38 @@ Created on Mon Oct 31 14:04:46 2016
 """
 
 import pandas as pd
-import numpy as np
 import datetime as dt
-import sklearn as skl
-
 from load_data import load_data
-
 from learn_xgb import learn_xgb
-from evaluation import evalerror
 
 print("Importing the data...")
-features_data, rcvcall_data = load_data("/home/nicolas/Documents/INF554 - Machine Learning/AXA Data Challenge/train_2011_2012_2013.7z/train_2011_2012_2013.csv")
+
+ass = ['CMS', 'Crises', 'Domicile', 'Gestion', 'Gestion - Accueil Telephonique',
+           'Gestion Assurances', 'Gestion Relation Clienteles', 'Gestion Renault',
+           'Japon', 'Médical', 'Nuit', 'RENAULT', 'Regulation Medicale', 'SAP', 
+           'Services', 'Tech. Axa', 'Tech. Inter', 'Téléphonie']
+l = len(ass)
+
+features_data, rcvcall_data = load_data("/home/nicolas/Documents/INF554 - Machine Learning/AXA Data Challenge/train_2011_2012_2013.7z/train_2011_2012_2013.csv",
+                                        ass)
 
 
 ## Learning algorithm 
 print("Fitting the data...")
-model, error = learn_xgb(features_data, rcvcall_data)
+models = list(range(l))
+for i in range(l):
+    print("Fitting n°" + str(i + 1) + "/" + str(l))
+    models[i], error = learn_xgb(features_data[i], rcvcall_data[i])
 
 
 ## Result visualization 
 
-X_test = features_data[features_data.index < dt.datetime(2011, 2, 16)]
-y_test = rcvcall_data[rcvcall_data.index < dt.datetime(2011, 2, 16)]
+n_to_vis = 3
+
+X_test = features_data[n_to_vis][features_data[n_to_vis].index < dt.datetime(2011, 2, 16)]
+y_test = rcvcall_data[n_to_vis][rcvcall_data[n_to_vis].index < dt.datetime(2011, 2, 16)]
                                                                          
-y_pred = pd.DataFrame(model.predict(X_test))
+y_pred = pd.DataFrame(models[n_to_vis].predict(X_test))
 
 y_pred.index = y_test.index
 y_test = pd.DataFrame(y_test)
@@ -38,7 +46,4 @@ y = pd.concat([y_test, y_pred], axis = 1)
 y.columns = ["TRUE_VALUE", "PRED_VALUE"]
 
 y.plot()
-
-
-
 
