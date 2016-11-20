fc3543761bb646160fa116a1ede0b2c98e3d9962
NicoRahm <nicolas.rahmouni@polytechnique.edu>
2016-11-20 02:57:12 +0100

    Adding latest changes of dependancies

--- main.py
+++ main.py
@@ -9,17 +9,23 @@ import pandas as pd
 import datetime as dt
 from load_data import load_data
 from learn_xgb import learn_xgb
+import os
+import saveload_models as slmod
+
+os.chdir("/home/nicolas/Documents/INF554 - Machine Learning/AXA Data Challenge")
+name = input("Enter the name of the models to save (Enter to skip saving):")
 
 print("Importing the data...")
 
-ass = ['CMS', 'Crises', 'Domicile', 'Gestion', 'Gestion - Accueil Telephonique',
-           'Gestion Assurances', 'Gestion Relation Clienteles', 'Gestion Renault',
-           'Japon', 'Médical', 'Nuit', 'RENAULT', 'Regulation Medicale', 'SAP', 
-           'Services', 'Tech. Axa', 'Tech. Inter', 'Téléphonie']
+ass = ['CMS', 'Crises', 'Domicile', 'Gestion', 'Gestion - Accueil Telephonique', 
+	'Gestion Assurances', 'Gestion Relation Clienteles', 'Gestion Renault', 'Japon', 'Médical',
+	 'Nuit', 'RENAULT', 'Regulation Medicale', 'SAP', 'Services', 'Tech. Axa', 'Tech. Inter', 'Téléphonie', 
+	 'Tech. Total', 'Mécanicien', 'CAT', 'Manager', 'Gestion Clients', 'Gestion DZ', 'RTC', 'Prestataires']
 l = len(ass)
 
-features_data, rcvcall_data, preproc_data = load_data("/home/nicolas/Documents/INF554 - Machine Learning/AXA Data Challenge/train_2011_2012_2013.7z/train_2011_2012_2013.csv",
-                                        ass)
+features_data, rcvcall_data, preproc_data = load_data("train_2011_2012_2013.csv",
+                                                      ass,
+                                                      20000)
 
 
 ## Learning algorithm 
@@ -32,16 +38,23 @@ for i in range(l):
 
 models = pd.DataFrame(models)
 models.index = ass
-models.rename(index = str, columns = {0:"Models"})
+models = models.rename(index = str, columns = {0:"Models"})
+
+if (name != ''):
+    print("Saving models...")
+    slmod.save_multiple_xgb(models, name)
     
 ## Result visualization 
 
+print("Visualizing results...")
+
 n_to_vis = 3
+ass_to_vis = ass[n_to_vis]
 
 X_test = features_data[n_to_vis][features_data[n_to_vis].index < dt.datetime(2011, 2, 16)]
 y_test = rcvcall_data[n_to_vis][rcvcall_data[n_to_vis].index < dt.datetime(2011, 2, 16)]
                                                                          
-y_pred = pd.DataFrame(models[n_to_vis].predict(X_test))
+y_pred = pd.DataFrame(models.loc[ass_to_vis]['Models'].predict(X_test))
 
 y_pred.index = y_test.index
 y_test = pd.DataFrame(y_test)
